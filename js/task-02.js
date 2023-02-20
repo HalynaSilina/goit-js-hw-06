8425bf0fde399709c6bfc2644828227ecb275e18
HalynaSilina <halynasilina@gmail.com>
2023-02-20 14:16:17 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listRef = document.querySelector('#ingredients');
+
+const listOfIngredients = ingredients.map(ingredient => {
+  const listElem = document.createElement('li');
+  listElem.textContent = ingredient;
+  listElem.classList.add("item");
+  return listElem;
+})
+
+listRef.append(...listOfIngredients);
