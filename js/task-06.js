ca1ef70255d234e08274c036251c5ee49a3b87f1
HalynaSilina <halynasilina@gmail.com>
2023-02-21 00:01:15 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const inputRef = document.querySelector("#validation-input");
+
+inputRef.addEventListener("blur", inputBlurValidationHandler);
+
+function inputBlurValidationHandler() {
+  if (inputRef.value.length === Number(inputRef.dataset.length)) {
+    inputRef.classList.add("valid");
+    inputRef.classList.remove("invalid");
+  } else {
+    inputRef.classList.remove("valid");
+    inputRef.classList.add("invalid");
+  }
+}
