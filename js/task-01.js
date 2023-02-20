6761d4ca4bc55ef072fbe9b9645ee77d60a83d02
HalynaSilina <halynasilina@gmail.com>
2023-02-20 13:09:39 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesRefs = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesRefs.length}`);
+
+categoriesRefs.forEach((category) => {
+    const categoryTitle = category.querySelector('h2').textContent;
+    const categoryElements = category.querySelectorAll('li').length;
+    console.log(`Category: ${categoryTitle}`);
+    console.log(`Elements: ${categoryElements}`);
+  });
