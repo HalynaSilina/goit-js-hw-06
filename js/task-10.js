618ef507d328181b8e73b472e38c410f8d3bf7bd
HalynaSilina <halynasilina@gmail.com>
2023-02-21 15:55:41 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,39 @@
+const amountInput = document.querySelector("#controls input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesRef = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", createBoxesBtnClickHandler);
+destroyBtn.addEventListener("click", destroyBoxesBtnClickHandler);
+
+function createBoxesBtnClickHandler() {
+  const amount = Number(amountInput.value);
+  const newBoxes = createBoxes(amount);
+  boxesRef.append(...newBoxes);
+}
+function destroyBoxesBtnClickHandler() {
+  destroyBoxes();
+  amountInput.value = "";
+}
+function createBoxes(amount) {
+  const boxes = [];
+  let width = 30;
+  let height = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${width}px`;
+    box.style.height = `${height}px`;
+    console.log(box);
+    boxes.push(box);
+    width += 10;
+    height += 10;
+  }
+  return boxes;
+}
+function destroyBoxes() {
+  boxesRef.innerHTML = "";
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
