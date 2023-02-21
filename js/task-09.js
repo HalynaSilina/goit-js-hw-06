f8e625f361abd78703dc8fd8ae41facaad61be38
HalynaSilina <halynasilina@gmail.com>
2023-02-21 14:12:12 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const bodyBackground = document.querySelector('body');
+const backgroundColorName = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', changeColorBtnclickHandler);
+
+function changeColorBtnclickHandler() {
+  const color = getRandomHexColor();
+  bodyBackground.style.backgroundColor = color;
+  backgroundColorName.textContent= color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
