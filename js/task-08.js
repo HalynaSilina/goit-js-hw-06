8ec31990dd4f3fed48cdb07b21c3feb0e3fde4ff
HalynaSilina <halynasilina@gmail.com>
2023-02-21 13:55:39 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", formSubmitHandler);
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === "" || password === "") {
+   return window.alert("You need to fill in all fields");
+  }
+  const formData = {
+    userEmail: email,
+    userPassword: password,
+  };
+  console.log(formData);
+  event.currentTarget.reset();
+}
